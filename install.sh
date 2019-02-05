8ae2d5f63ae625a6bb6d82063d780ffa9b2ee53f
Shunji Zhan <shunhahaha@live.com>
2019-02-05 20:24:25 +0800

    polished build.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
-echo "installing node modules for server..."
+echo "
+----------------------------------------
+฿ installing node modules for server ...
+----------------------------------------"
 cd server/
 npm install
 
-echo "installing node modules for frontend..."
+
+echo "
+--------------------------------------------------------------
+฿ installing node modules for React, this may take a while ...
+--------------------------------------------------------------"
 cd ../view/
 npm install
 
-echo "installing general node modules ..."
+
+echo "
+-------------------------------------
+฿ installing general node modules ...
+-------------------------------------"
 cd ..
 npm install
+
+
+echo "
+------------------------------------
+฿ all node modules were installed !!
+------------------------------------"
