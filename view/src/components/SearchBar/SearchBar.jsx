878ce0ad1a949a4475af978e112e7f29b6ad7c38
Shunji Zhan <shunhahaha@live.com>
2019-02-24 17:11:29 -0800

    small search bar style polish

--- view/src/components/SearchBar/SearchBar.jsx
+++ view/src/components/SearchBar/SearchBar.jsx
@@ -9,7 +9,7 @@ class SearchBar extends Component {
     render() {
         return (
         <div id='searchBar'>
-            <input type="text" id="searchText" placeholder="What are you looking for?" />
+            <input type="text" id="searchText" placeholder="search in over 1000000+ medical case reports..." />
             <button type="submit" id="searchButton">
                 <FontAwesomeIcon icon={['fas', 'search']} />
             </button>
