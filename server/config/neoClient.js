6fb93f38856e9944c2745a1406a54ddc28f0854a
ZixiaWeng <zixia@g.ucla.edu>
2019-04-23 17:25:20 -0700

    change neo4j client

--- server/config/neoClient.js
+++ server/config/neoClient.js
@@ -1,5 +1,6 @@
 var neo4j = require('neo4j-driver').v1;
-var driver = new neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "ucla"));
+var driver = new neo4j.driver("bolt://34.229.254.166:33758", neo4j.auth.basic("neo4j", "uncertainties-habits-documentations"));
+// var driver = new neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "ucla"));
 
 var getSession = function (context) {
   if (context.neo4jSession) {
