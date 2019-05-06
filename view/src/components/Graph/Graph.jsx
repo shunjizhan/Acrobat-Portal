789b914396a0b2856432f48f9a40e3ac604b6932
Don <donlee940125@gmail.com>
2019-05-06 12:42:44 -0700

    now showing two level of neighbors of query words in subgraph

--- view/src/components/Graph/Graph.jsx
+++ view/src/components/Graph/Graph.jsx
@@ -3,6 +3,7 @@ import Cytoscape from 'cytoscape';
 import CytoscapeComponent from 'react-cytoscapejs';
 import Dagre from 'cytoscape-dagre';
 import {buildGraphElementsFromGraphData} from '../../utils';
+import {buildSubGraphElementsFromGraphData} from '../../utils';
 
 Cytoscape.use(Dagre);
 
@@ -17,7 +18,7 @@ class Graph extends Component {
     
     // Create the Elements (Nodes) to be visualized
     var elements = buildGraphElementsFromGraphData(graphData);
-
+    var sub_elements = buildSubGraphElementsFromGraphData(graphData, entities);
     // Layout
     const layout = {
       name: 'dagre',
@@ -36,11 +37,10 @@ class Graph extends Component {
           "content": "data(label)",
           "text-valign": "center",
           "text-halign": "center",
-          // "text-font-size": 100,
           "width": 60,
           "height": 60,
           "shape": "data(type)",
-          "background-color": "rgb(244, 149, 65)",              // node color
+          "background-color": "data(color)",               // node color
           "text-outline-color": "rgb(244, 149, 65)",
           "text-outline-opacity": 1,
           "text-outline-width": 0,
@@ -52,7 +52,10 @@ class Graph extends Component {
         "selector": "edge",
         "style": {
           "width": 3,
-          "curve-style": "straight"
+          "curve-style": "straight",
+          'label': 'data(label)',
+          "font-size": 10,
+          "length": 10
         }
       },
       {
@@ -66,13 +69,20 @@ class Graph extends Component {
     
     return (
       <div>
-      <div>
-      <CytoscapeComponent 
-        elements={elements} 
-        style={ { width: '100%', height: '500px'}} 
-        layout={layout} 
-        stylesheet={stylesheet} ></CytoscapeComponent>
-      </div>
+        <div>
+          <CytoscapeComponent 
+            elements={sub_elements} 
+            style={ { width: '100%', height: '500px'}} 
+            layout={layout} 
+            stylesheet={stylesheet} ></CytoscapeComponent>
+        </div>
+        <div>
+          <CytoscapeComponent 
+            elements={elements} 
+            style={ { width: '100%', height: '500px'}} 
+            layout={layout} 
+            stylesheet={stylesheet} ></CytoscapeComponent>
+        </div>
       </div>
     );
   }
