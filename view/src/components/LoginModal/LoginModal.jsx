9ec3d235012c5cab64e7738ed3da12c290ceeb21
Shunji Zhan <shunhahaha@live.com>
2019-05-22 15:30:07 -0700

    able to switch between signup and login page

--- view/src/components/LoginModal/LoginModal.jsx
+++ view/src/components/LoginModal/LoginModal.jsx
@@ -1,12 +1,95 @@
 import React, { Component } from 'react';
 import Modal from 'react-awesome-modal';
+import './LoginModal.css';
 
-export default class LoginModal extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            visible : true
+
+class LoginContent extends Component {
+    render() {
+        const { handleCloseModal, switchAction } = this.props;
+
+        return (
+            <div className='modal-inner-content'>
+                <h1>Login</h1>
+                <p>This is the login page</p>
+
+                <button onClick={ handleCloseModal }>
+                    Close
+                </button>
+
+                <button onClick={ switchAction }>
+                    sign up
+                </button>
+            </div>
+        );
+    }
+}
+
+
+class SignUpContent extends Component {
+    render() {
+        const { handleCloseModal, switchAction } = this.props;
+
+        return (
+            <div className='modal-inner-content'>
+                <h1>Sign Up</h1>
+                <p>This is the sign up </p>
+
+                <button onClick={ handleCloseModal }>
+                    Close
+                </button>
+
+                <button onClick={ switchAction }>
+                    login
+                </button>
+            </div>
+        );
+    }
+}
+
+
+
+class ModalContent extends Component {
+    state = {
+        currentAction: 'login'      // either login or signup
+    }
+
+    switchAction = () => {
+        this.setState(prevState => {
+            const newAction = 
+                prevState.currentAction === 'login'? 
+                'signup' : 'login';
+
+            return {
+                currentAction: newAction
+            };
+        })
+    }
+
+    render() {
+        const { handleCloseModal } = this.props;
+        const { currentAction } = this.state;
+
+        let content;
+        if (currentAction === 'login') {
+            content = <LoginContent 
+                        handleCloseModal={ handleCloseModal }
+                        switchAction={ this.switchAction }
+                      />
+        } else {
+            content = <SignUpContent 
+                        handleCloseModal={ handleCloseModal }
+                        switchAction={ this.switchAction }
+                      />
         }
+
+        return content;
+    }
+}
+
+
+class LoginModal extends Component {
+    state = {
+        visible : true
     }
 
     openModal = () => {
@@ -21,27 +104,24 @@ export default class LoginModal extends Component {
         const { visible } = this.state;
 
         return (
-            <section>
+            <div id='login-modal'>
                 <h1>React-Modal Examples</h1>
                 <input type="button" value="Open" onClick={() => this.openModal()} />
                 <Modal 
                     visible={ visible } 
-                    width="400" 
-                    height="300" 
+                    width="600" 
+                    height="500" 
                     effect="fadeInDown" 
                     onClickAway={ this.closeModal }
                 >
-                    <div>
-                        <h1>Title</h1>
-                        <p>Some Contents</p>
-                        <button
-                            onClick={ this.closeModal }
-                        >
-                            Close
-                        </button>
-                    </div>
+                    <ModalContent 
+                        handleCloseModal={ this.closeModal }
+                    />
                 </Modal>
-            </section>
+            </div>
         );
     }
 }
+
+
+export default LoginModal;
